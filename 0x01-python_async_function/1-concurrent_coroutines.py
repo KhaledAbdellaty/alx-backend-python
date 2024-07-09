00b27a49f26b4d18dba6ced92091f373107c23f9
KhaledAbdellaty <kabdellaty@gmail.com>
2024-07-09 03:29:14 +0300

    2. Measure the runtime

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,5 +7,5 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """wait_n should return the list of all the delays (float values)."""
-    todos = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
-    return [await todo for todo in asyncio.as_completed(todos)]
+    tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
---
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''An asynchronous coroutine '''
+import asyncio
+import time
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    '''A function that measures the total execution time
+    for wait_n(n, max_delay), and returns total_time / n
+    '''
+    start_at = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    return (time.time() - start_at) / n
